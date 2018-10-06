19ed95f4b1441794f3c6b111ba5fb31f2f68dd81
AlexEchavarria <43913675+AlexEchavarria@users.noreply.github.com>
2018-10-06 17:40:36 -0500

    Update Is_bigger_smarter.java
    Se hace pequeño cambio en el codigo ya que solo nos faltaba un 0.

--- Is_bigger_smarter.java
+++ Is_bigger_smarter.java
@@ -2,7 +2,8 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
- */
+ *@autor Banilat, 
+*/
 package is_bigger_smarter;
 
 import java.io.BufferedReader;
@@ -22,7 +23,7 @@ public class Is_bigger_smarter {
     public static class Elefante implements Comparable<Elefante>{
         int peso, iq, id;
         Elefante(){
-            this.peso=0;
+            this.peso=-1;
             this.id=0;
             this.iq=0;
         }
@@ -39,15 +40,15 @@ public class Is_bigger_smarter {
     }
     public static void main(String[] args) throws IOException {
         
-        Elefante e[]= new Elefante[1000];
-        for(int i=0; i<1000;i++){
+        Elefante e[]= new Elefante[10000];
+        for(int i=0; i<10000;i++){
             e[i]=new Elefante();
         }
         int cant=0;
         
         BufferedReader bf= new BufferedReader(new InputStreamReader(System.in));
         String linea;
-        for(int i=0; i<1000; i++){
+        for(int i=0; i<10000; i++){
             linea=bf.readLine();
             if("".equals(linea) || linea==null){
                 break;
@@ -60,19 +61,18 @@ public class Is_bigger_smarter {
             e[i]=ele;
         }
         Arrays.sort(e);
-        for (int i=0; i<1000; i++){
-            if(e[i].peso!=0){
-                break;
-            }else{
+        for (int i=0; i<10000; i++){
+            if(e[i].peso!=-1){
                 cant=i;
+                break;
             }
         }
         System.out.println(cant);
         int[] exist= new int[e.length];
-        exist[cant+1]=1;
-        for(int i=cant+2;i<exist.length ;i++){
+        exist[cant]=1;
+        for(int i=cant;i<exist.length ;i++){
             exist[i]=Math.max(1, exist[i]);
-            for (int j=cant+1; j<i;j++){
+            for (int j=cant; j<=i;j++){
                 if(e[i].iq<e[j].iq && e[i].peso>e[j].peso){
                     exist[i]=Math.max(exist[j]+1,exist[i] );
                     
